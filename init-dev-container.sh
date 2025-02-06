67257f30037bc5452ed327d62bfe5bdaece4d0de
Max Otta <max.otta@gmail.com>
2025-02-06 01:19:01 +0100

    Locale bugfix (Ansible requires the locale encoding to be UTF-8)

--- init-dev-container.sh
+++ init-dev-container.sh
@@ -1,7 +1,7 @@
 test -f /root/.ssh || mkdir -p /root/.ssh ; chmod 700 /root/.ssh
 cp -f ${PROJECT_DIR}/config/id_* /root/.ssh
 
-export LC_ALL="C"
+export LC_ALL="C.UTF-8"
 export PATH=$PATH:/usr/games
 
 set -a
